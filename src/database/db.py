09f8736795b4b54ac3e5b5f6077a6cb65fd2b5a4
Your Name <you@example.com>
2025-06-28 19:56:32 +0300

    alembic fix

--- src/database/db.py
+++ src/database/db.py
@@ -1,7 +1,10 @@
+import logging
+
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from src.config.settings import settings
 
 
 engine = create_async_engine(settings.db_url, echo=False)
 AsyncDBSession = async_sessionmaker(engine, expire_on_commit=False)
+logging.info("USING DB: %s", settings.db_url)
 
