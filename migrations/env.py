df23a405f1301bccbcd2796890421c757c77b0a6
Your Name <you@example.com>
2025-06-28 19:51:54 +0300

    alembic fix

--- migrations/env.py
+++ migrations/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlalchemy.ext.asyncio import async_engine_from_config, create_async_engine
 
 from alembic import context
 
@@ -71,9 +71,8 @@ async def run_async_migrations() -> None:
 
     """
 
-    connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+    connectable = create_async_engine(
+        settings.db_url,
         poolclass=pool.NullPool,
     )
 
